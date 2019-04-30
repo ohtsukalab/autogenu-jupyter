3036a295eff8720e89fd4763f58f6c5cd067656e
Sotaro Katayama <ihtf4ta7f@gmail.com>
2019-04-30 12:38:12 +0900

    [C++]modify for windows

--- AutoGenU_modules/cpp_executor.py
+++ AutoGenU_modules/cpp_executor.py
@@ -23,15 +23,15 @@ def makeAndRun(simulation_name):
         for line in iter(proc.stdout.readline,b''):
             print(line.rstrip().decode("utf8"))
         print('\n')
-        proc = subprocess.Popen(['MSBuild', 'ALL_BUILD.vcxproj'], cwd='models/'+simulation_name+'/build', stdout = subprocess.PIPE, stderr = subprocess.STDOUT, shell=True)
+        proc = subprocess.Popen(['MSBuild', 'ALL_BUILD.vcxproj'], cwd='models/'+simulation_name+'/build/Release', stdout = subprocess.PIPE, stderr = subprocess.STDOUT, shell=True)
         for line in iter(proc.stdout.readline,b''):
             print(line.rstrip().decode("utf8"))
         print('\n')
-        proc = subprocess.run(['main.exe'], cwd='models/'+simulation_name+'/build', stdout = subprocess.PIPE, stderr = subprocess.STDOUT, shell=True)
+        proc = subprocess.run(['main.exe'], cwd='models/'+simulation_name+'/build/Release', stdout = subprocess.PIPE, stderr = subprocess.STDOUT, shell=True)
         for line in iter(proc.stdout.readline,b''):
             print(line.rstrip().decode("utf8"))
         subprocess.run(['rm', '-r', 'simulation_result'], cwd='models/'+simulation_name, stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell=True)
-        subprocess.run(['mv', 'simulation_result', '../'], cwd='models/'+simulation_name+'/build', stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell=True)
+        subprocess.run(['mv', 'simulation_result', '../'], cwd='models/'+simulation_name+'/build/Release', stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell=True)
     else:
         proc = subprocess.Popen(['cmake', '--build', '.'], cwd='models/'+simulation_name+'/build', stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
         for line in iter(proc.stdout.readline,b''):
