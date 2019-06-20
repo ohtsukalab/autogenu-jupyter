8db4a3c15512f15244471b2ecd3dbab44c4752a0
sotaro <ihtf4ta7f@gmail.com>
2019-06-21 01:29:15 +0900

    [Python]modify AutoGenU

---
+++ AutoGenU/simulation_params.py
@@ -0,0 +1,6 @@
+class SimulationParams:
+    def __init__(self, initial_time, initial_state, simulation_time, sampling_time):
+        self.initial_time = initial_time
+        self.initial_state = initial_state
+        self.simulation_time = simulation_time
+        self.sampling_time = sampling_time
