7ba5816583a2ebfdde13d80a01cb01a3c63fd483
mayataka <ihtf4ta7f@gmail.com>
2019-11-24 14:37:18 +0900

    modify

--- autogenu/animation_generator.py
+++ autogenu/animation_generator.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 import mpl_toolkits.mplot3d as a3
 import mpl_toolkits.mplot3d.art3d as art3d 
 from matplotlib.animation import FuncAnimation
@@ -434,3 +435,171 @@ class Hexacopter(object):
             for i in range(6)
         ]
         return X_w, Y_w, Z_w
+
+
+class MobileRobot(object):
+    """ Generates the animation of the simulation results of a cart pole.
+
+        Attributes: 
+            set_skip_frames(skip_frames): Sets how many frames you want to 
+                skip in generating the animation. In the default settings, 
+                skip_frames = 1.
+            generate_animation(): Draws an animation of the simulation reult
+                and saves it as a .mp4 files.
+    """
+
+    def __init__(self, model_name, vx_ref, X1, Y1, R1, X2, Y2, R2):
+        """ Inits CartPole with loading the simulation results. """
+        # Loads the simulation data.
+        self.__model_dir = 'models/' + model_name + '/simulation_result' 
+        self.__file_header = self.__model_dir + '/' + model_name
+        self.__state_data = np.genfromtxt(
+            self.__file_header+'_state'+'.dat'
+        )
+        self.__sim_conditions = simcon.SimulationConditions(
+            self.__file_header
+        )
+        self.__time_sequence = np.linspace(
+            0, 
+            self.__sim_conditions.simulation_time(), 
+            self.__state_data.shape[0]
+        )
+        # Replaces NaN with 0.
+        self.__state_data[np.isnan(self.__state_data)] = 0
+        # Checks the dimension of the state.
+        self.__dim_state = self.__state_data.shape[1]
+        if self.__dim_state != 3:
+            print(
+                'Dimension of the state is not 3!\n'
+                'This may not be data for simulation of a mobile robot\n'
+            )
+            sys.exit() 
+        # Sets the drawing range.
+        self.__robot_length = 0.2
+        self.__robot_width = 0.15
+        hlength = 0.5 * self.__robot_length
+        hwidth = 0.5 * self.__robot_width
+        margin = np.sqrt(hlength**2 + hwidth**2)
+        self.__x_min = np.min(self.__state_data[:, 0]) - margin
+        self.__x_max = np.max(self.__state_data[:, 0]) + margin
+        self.__y_min = np.min(self.__state_data[:, 1]) - margin
+        self.__y_max = np.max(self.__state_data[:, 1]) + margin
+        # Sets reference trajectory.
+        self.__vx_ref = vx_ref
+        # Sets obstacles.
+        self.__X1, self.__Y1, self.__R1 = X1, Y1, R1-margin
+        self.__X2, self.__Y2, self.__R2 = X2, Y2, R2-margin
+        # Sets frames for drawing the animation.
+        self.__skip_frames = 1
+        self.__total_frames = (int)(
+            self.__state_data.shape[0]/self.__skip_frames
+        )
+
+    def set_skip_frames(self, skip_frames):
+        """ Set how many frames you want to skip in generating the animation.
+
+            Args:
+                skip_frames: A number of frames to skip.
+        """
+        self.__skip_frames = skip_frames
+        self.__total_frames = (int)(
+            self.__state_data.shape[0]/skip_frames
+        )
+
+    def generate_animation(self):
+        """ Generates the animation and saves it as a .mp4 file. """
+        margin = max(self.__robot_length, self.__robot_width)
+        self.__fig = plt.figure(figsize=(10, 5))
+        xrange = self.__x_max - self.__x_min
+        yrange = self.__y_max - self.__y_min
+        max_range = max(xrange, yrange) + margin
+        self.__ax = plt.axes(
+            xlim=(self.__x_min-margin, self.__x_min+max_range), 
+            ylim=(-0.5*(5/10)*max_range, 0.5*(5/10)*max_range)
+        )
+        obs1 = patches.Circle(xy=(self.__X1, self.__Y1), radius=self.__R1, 
+                                  fc='w', ec='gray', linewidth=3)
+        obs2 = patches.Circle(xy=(self.__X2, self.__Y2), radius=self.__R2, 
+                                  fc='w', ec='gray', linewidth=3)
+        self.__ax.add_patch(obs1)
+        self.__ax.add_patch(obs2)
+        self.__line1, = self.__ax.plot([], [], color='#0063B1', linewidth=5)
+        self.__line2, = self.__ax.plot([], [], color='#0063B1', linewidth=5)
+        self.__line3, = self.__ax.plot([], [], color='#0063B1', linewidth=5)
+        self.__line4, = self.__ax.plot([], [], color='#0063B1', linewidth=5)
+        self.__ref, = self.__ax.plot([], color='orange', marker='.',
+                                     markersize=20)
+        self.__ax.tick_params(
+            labelbottom=False, 
+            labelleft=False, 
+            labelright=False, 
+            labeltop=False
+        )
+        self.__ax.tick_params(color='white')
+        self.__time_text = self.__ax.text(
+            0.85, 0.05, 
+            '', 
+            transform=self.__ax.transAxes, 
+            fontsize=14
+        )
+        # Generates an animation.
+        anime = FuncAnimation(
+            self.__fig, 
+            self.__update_animation, 
+            interval=self.__sim_conditions.sampling_period()*1000, 
+            frames=self.__total_frames, 
+            blit=True
+        )
+        anime.save(
+            self.__file_header+'.mp4', 
+            writer='ffmpeg', 
+            fps = int(
+                1/(self.__sim_conditions.sampling_period()*self.__skip_frames)
+            )
+        )
+        print(
+            'The animation of the simlation results is generated at '
+            +self.__file_header
+            +'.mp4\n'
+        )
+
+    def __update_animation(self, i):
+        frame = self.__skip_frames * i
+        p_fl, p_fr, p_bl, p_br = self.__robot_world(self.__state_data[frame, :])
+        self.__line1.set_data((p_fl[0], p_fr[0]), (p_fl[1], p_fr[1]))
+        self.__line2.set_data((p_fl[0], p_bl[0]), (p_fl[1], p_bl[1]))
+        self.__line3.set_data((p_fr[0], p_br[0]), (p_fr[1], p_br[1]))
+        self.__line4.set_data((p_bl[0], p_br[0]), (p_bl[1], p_br[1]))
+        self.__ref.set_data(self.__get_time(i)*self.__vx_ref, 0)
+        self.__time_text.set_text(
+            '{0:.1f} [s]'.format(self.__sim_conditions.sampling_period()*frame)
+        )
+        return (
+            self.__line1, self.__line2, self.__line3, self.__line4, self.__ref,
+            self.__time_text
+        )
+
+    def __robot_world(self, x):
+        hlength = 0.5*self.__robot_length
+        hwidth = 0.5*self.__robot_width
+        # Configurations in the world frame
+        p_fl = (
+            x[0] + hlength * np.cos(x[2]) - hwidth * np.sin(x[2]),
+            x[1] + hlength * np.sin(x[2]) + hwidth * np.cos(x[2])
+        )
+        p_fr = (
+            x[0] + hlength * np.cos(x[2]) + hwidth * np.sin(x[2]),
+            x[1] + hlength * np.sin(x[2]) - hwidth * np.cos(x[2])
+        )
+        p_bl = (
+            x[0] - hlength * np.cos(x[2]) - hwidth * np.sin(x[2]),
+            x[1] - hlength * np.sin(x[2]) + hwidth * np.cos(x[2])
+        )
+        p_br = (
+            x[0] - hlength * np.cos(x[2]) + hwidth * np.sin(x[2]),
+            x[1] - hlength * np.sin(x[2]) - hwidth * np.cos(x[2])
+        )
+        return p_fl, p_fr, p_bl, p_br
+
+    def __get_time(self, i):
+        return i*self.__sim_conditions.simulation_time()/self.__total_frames
