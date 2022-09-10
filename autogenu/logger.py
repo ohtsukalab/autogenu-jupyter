02e56059829f8830430f0fa0fcd7d175e4a16621
Sotaro Katayama <ihtf4ta7f@gmail.com>
2022-09-10 11:48:35 +0900

    add Python log examples

--- autogenu/logger.py
+++ autogenu/logger.py
@@ -8,15 +8,21 @@ class Logger(object):
         self.x_log = open(os.path.join(log_dir, log_name+"_x.log"), mode='w')
         self.u_log = open(os.path.join(log_dir, log_name+"_u.log"), mode='w')
         self.opterr_log = open(os.path.join(log_dir, log_name+"_opterr.log"), mode='w')
+        self.open = True
+
+    def __del__(self):
+        self.close()
 
     def save(self, t: float, x: np.ndarray, u: np.ndarray, opterr: float):
-        np.savetxt(self.t_log, np.array([t]))
-        np.savetxt(self.x_log, [x])
-        np.savetxt(self.u_log, [u])
-        np.savetxt(self.opterr_log, np.array([opterr]))
+        if self.open:
+            np.savetxt(self.t_log, np.array([t]))
+            np.savetxt(self.x_log, [x])
+            np.savetxt(self.u_log, [u])
+            np.savetxt(self.opterr_log, np.array([opterr]))
 
     def close(self):
         self.t_log.close()
         self.x_log.close()
         self.u_log.close()
         self.opterr_log.close()
+        self.open = False
---
+++ examples/python/mobilerobot_log.py
@@ -0,0 +1,61 @@
+import cgmres.mobilerobot
+import cgmres.common
+import numpy as np
+
+
+ocp = cgmres.mobilerobot.OCP()
+
+horizon = cgmres.common.Horizon(Tf=1.5) # fixed length
+
+settings = cgmres.common.SolverSettings()
+settings.sampling_time = 0.001
+settings.zeta = 1000
+settings.finite_difference_epsilon = 1e-08
+settings.max_iter = 50
+settings.opterr_tol = 1e-06
+settings.verbose_level = 1 # print opt error
+
+t0 = 0.0
+x0 = np.zeros(ocp.nx)
+
+# Initialize solution using zero horizon OCP solution
+initializer = cgmres.mobilerobot.ZeroHorizonOCPSolver(ocp, settings)
+uc0 = np.array([0.1, 0.1, 0.01, 0.01])
+initializer.set_uc(uc0)
+initializer.solve(t0, x0)
+
+# Create MPC solver and set the initial solution 
+mpc = cgmres.mobilerobot.MultipleShootingCGMRESSolver(ocp, horizon, settings)
+mpc.set_uc(initializer.ucopt)
+mpc.init_x_lmd(t0, x0)
+mpc.init_dummy_mu()
+
+from autogenu import Logger
+logger = Logger(log_dir='logs/mobilerobot', log_name='mobilerobot')
+
+# simple simulation with forward Euler 
+tsim = 10.0
+sampling_time = settings.sampling_time
+t = t0
+x = x0.copy()
+for _ in range(int(tsim/sampling_time)):
+    u = mpc.uopt[0]
+    dx = ocp.eval_f(t, x, u)
+    x1 = x + sampling_time * dx
+    mpc.update(t, x)
+
+    logger.save(t, x, u, mpc.opt_error())
+
+    x = x1
+    t = t + sampling_time
+    print('t: ', t, ', x: ', x)
+
+logger.close()
+
+print("\n======================= MPC used in this simulation: =======================")
+print(mpc)
+
+from autogenu import Plotter
+plotter = Plotter(log_dir='logs/mobilerobot', log_name='mobilerobot')
+plotter.show()
+plotter.save()
